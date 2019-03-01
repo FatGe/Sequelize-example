e46e4e6c56911961b11cef46d90bd74cd5901432
FatGe <duangci@aliyun.com>
2019-03-01 13:43:40 +0800

    base build chapter-two

--- index.js
+++ index.js
@@ -1,47 +1,59 @@
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('database', 'username', 'password', {
-    host: '',
-    port: '',
-    dialect: 'mysql',
-    operatorsAliases: false,
-    logging: false,
+// const sequelize = new Sequelize('users_dev', 'gyz', 'Geyizhongzui7==', {
+//     host: '134.175.208.242',
+//     port: 3306,
+//     dialect: 'mysql',
+//     operatorsAliases: false,
+//     logging: false,
 
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
-});
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     },
+// });
 
-// sequelize
-//     .authenticate()
-//     .then(() => {
-//         console.log('Connection has been established successfully.');
-//     })
-//     .catch(err => {
-//         console.error('Unable to connect to the database:', err);
+// // sequelize
+// //     .authenticate()
+// //     .then(() => {
+// //         console.log('Connection has been established successfully.');
+// //     })
+// //     .catch(err => {
+// //         console.error('Unable to connect to the database:', err);
+// //     });
+
+// const User = sequelize.define('user', {
+//     firstName: {
+//         type: Sequelize.STRING
+//     },
+//     lastName: {
+//         type: Sequelize.STRING
+//     }
+// });
+
+// // force: true will drop the table if it already exists
+// User.sync({
+//     force: true
+// }).then(() => {
+//     // Table created
+//     return User.create({
+//         firstName: 'John',
+//         lastName: 'Hancock'
 //     });
+// }).then(() => {
+//     process.exit()
+// })
 
-const User = sequelize.define('user', {
-    firstName: {
-        type: Sequelize.STRING
-    },
-    lastName: {
-        type: Sequelize.STRING
-    }
-});
+if (process.env.NODE_ENV === 'production') {
+    require('env2')('./.env.prod');
+} else {
+    require('env2')('./.env');
+}
 
-// force: true will drop the table if it already exists
-User.sync({
-    force: true
-}).then(() => {
-    // Table created
-    return User.create({
-        firstName: 'John',
-        lastName: 'Hancock'
-    });
-}).then(() => {
-    process.exit()
-})
+const {
+    env
+} = process;
+
+console.log(env.DB_NAME)
